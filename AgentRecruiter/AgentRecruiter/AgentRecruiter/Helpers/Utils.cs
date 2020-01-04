a8299067c62eeb2a8ef4ca361b476ed2790a2834
Robert Sundström <robert.sundstrom@outlook.com>
2020-01-04 12:11:14 +0100

    Store service endpoint URL in appsettings.json

---
+++ AgentRecruiter/AgentRecruiter/AgentRecruiter/Helpers/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace AgentRecruiter.Helpers
+{
+    public static class Utils
+    {
+        public static void ExtractSaveResource(string filename, string location)
+        {
+            var a = Assembly.GetExecutingAssembly();
+            using (var resFilestream = a.GetManifestResourceStream(filename))
+            {
+                if (resFilestream != null)
+                {
+                    string full = Path.Combine(location, filename);
+
+                    using (var stream = File.Create(full))
+                    {
+                        resFilestream.CopyTo(stream);
+                    }
+
+                }
+            }
+        }
+    }
+}
--- AgentRecruiter/AgentRecruiter/AgentRecruiter/Startup.cs
+++ AgentRecruiter/AgentRecruiter/AgentRecruiter/Startup.cs
@@ -1,6 +1,8 @@
-﻿using AgentRecruiter.Services;
+﻿using AgentRecruiter.Helpers;
+using AgentRecruiter.Services;
 using AgentRecruiter.ViewModels;
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -8,6 +10,9 @@ using Microsoft.Extensions.Logging;
 using RecruitmentService.Client;
 
 using System;
+using System.IO;
+
+using Xamarin.Essentials;
 
 namespace AgentRecruiter
 {
@@ -15,7 +20,21 @@ namespace AgentRecruiter
     {
         public static App Init(Action<HostBuilderContext, IServiceCollection> nativeConfigureServices = null)
         {
+            string systemDir = FileSystem.CacheDirectory;
+
+            Utils.ExtractSaveResource("AgentRecruiter.appsettings.json", systemDir);
+            Utils.ExtractSaveResource("AgentRecruiter.appsettings.Development.json", systemDir);
+
+            string fullConfig = Path.Combine(systemDir, "AgentRecruiter.appsettings.json");
+            string fullDevConfig = Path.Combine(systemDir, "AgentRecruiter.appsettings.Development.json");
+
             var host = new HostBuilder()
+                            .ConfigureHostConfiguration(c =>
+                            {
+                                c.AddCommandLine(new string[] { $"ContentRoot={FileSystem.AppDataDirectory}" });
+                                c.AddJsonFile(fullConfig);
+                                c.AddJsonFile(fullDevConfig, optional: true);
+                            })
                             .ConfigureServices((c, x) =>
                             {
                                 nativeConfigureServices?.Invoke(c, x);
@@ -45,7 +64,14 @@ namespace AgentRecruiter
             services.AddSingleton<IDataService, DataService>();
             services.AddTransient<IRecruitmentQueryService, RecruitmentQueryService>();
 
-            services.AddHttpClient<IRecruitmentServiceClient, RecruitmentServiceClient>(httpClient => httpClient.BaseAddress = new Uri("https://v1.ifs.aero/"));
+            services.AddHttpClient<IRecruitmentServiceClient, RecruitmentServiceClient>((sp, httpClient) =>
+            {
+                var serviceEndpoint = sp
+                    .GetService<IConfiguration>()
+                    .GetValue<string>("ServiceEndpoint");
+
+                httpClient.BaseAddress = new Uri($"{serviceEndpoint}/");
+            });
         }
     }
 }
